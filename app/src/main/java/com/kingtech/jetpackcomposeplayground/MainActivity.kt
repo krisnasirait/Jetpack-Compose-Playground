7fb375cd688132a6f817c355d85c48b5ee9cc4db
Krisna Sirait <krisnapande00@outlook.com>
2022-10-29 09:49:37 +0700

    fully functional jetpack compose setup

--- app/src/main/java/com/kingtech/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/kingtech/jetpackcomposeplayground/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.kingtech.jetpackcomposeplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text(text = "Jetpack Compose")
+        }
     }
+
+
 }
