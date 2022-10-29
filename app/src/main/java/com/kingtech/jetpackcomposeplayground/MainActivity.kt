7a31f4d4981a55cadc3895f15c470545b342fa35
Krisna Sirait <krisnapande00@outlook.com>
2022-10-29 10:47:02 +0700

    test on preview and parameter on compose

--- app/src/main/java/com/kingtech/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/kingtech/jetpackcomposeplayground/MainActivity.kt
@@ -5,13 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Jetpack Compose")
+            MessageCard(name = "Krisna")
         }
+    }
+    
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        Text(text = "Hello World")
     }
 
 
